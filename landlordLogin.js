6f8a1889e8894ae5dcea08e3aa14b5df6860ec0e
R-kjim <rkayjim@gmail.com>
2024-07-17 13:23:04 +0300

    updated code

--- landlordLogin.js
+++ landlordLogin.js
@@ -2,16 +2,19 @@
 document.querySelector("#lordlogin").addEventListener("submit",loginFn)
 function loginFn(e){
     e.preventDefault()
-    fetch("http://localhost:3000/credentials")
+    fetch("http://localhost:3000/landlords")
     .then(res=>res.json())
     .then(data=>logFn(data))
     function logFn(item){
-        for(i=0;i<item.length;i++){
-        if(item[i].username.toLowerCase()===e.target.Username.value && item[i].password===e.target.password.value){
-            open("./landlordUserPage.html")
-        }else{
-            location.reload()
-            alert("Enter valid credentials")
+        for(let i=0;i<item.length;i++){
+            
+         item[i].username===(e.target.Username.value) &&item[i].password===(e.target.password.value)? open("./landlordUserPage.html"): location.reload(),alert("Enter valid credentials")
+
+
+            
+            
+           
         }
-    }}
-}
+   
+}}
+
